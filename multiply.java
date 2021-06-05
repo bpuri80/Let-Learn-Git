8dba5a8f0a17ed0e70f6a3bc612f1d4c5b144880
Bhuwan puri <puribhuwan80@gmail.com>
2021-06-05 23:37:38 +0530

    addded multiply.java

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 6;
+        int mul = a * b;
+        System.out.println("Multiply is :"+mul);
+    }
+    
+}
